2d6abef8ae3b7fe27a3f7cb3b79eca45ff4cb334
Lightnet <darkneter@gmail.com>
2018-04-05 21:44:00 -0700

    updated info.

--- README.md
+++ README.md
@@ -13,6 +13,8 @@
 # Information: 
   Simple prototype for running Phaser 3.3.0 on Lance-gg 2.0.5 multiplayer. Just add simple render and not add and remove object yet.
 
+  Phaser physics server side doens't work but depend on the npm lance-gg as another way to for 2D collision. This is just a test build.
+
 # Packages /Programs:
  * Nodejs 8.11.1 (run server)
  * Phaser 3.3.0 (Note this is typescript and javascript from package npm)
@@ -32,4 +34,7 @@
  * https://phaser.io/news/2015/05/ezgui
  * http://www.phaser.io/news/2017/08/phaser-ui-package
  * https://phaser.io/tutorials/making-your-first-phaser-3-game
- * 
+ * https://photonstorm.github.io/phaser3-docs/index.html
+ * http://www.emanueleferonato.com/2018/02/21/your-first-phaser-3-matter-js-physics-example/
+ * https://github.com/photonstorm/phaser3-examples/tree/master/public/src/physics/matterjs
+ * https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser/Physics
--- src/client/MyRenderer.js
+++ src/client/MyRenderer.js
@@ -28,22 +28,40 @@ export default class MyRenderer extends Renderer {
         this.sprites = {};
         //console.log(this);
     }
-
+    //Phaser
     preload(){
         super.preload();
-    }
+        this.load.setBaseURL('http://localhost:3000/');
 
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        this.load.image('red', 'assets/particles/red.png');
+    }
+    //Phaser
     create(){
         super.create();
         this.add.image(400, 300, 'sky');
 
+        var particles = this.add.particles('red');
+    
+        var emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
+        });
+    
+        //var logo = this.physics.add.image(400, 100, 'logo');
+    
+        //logo.setVelocity(100, 200);
+        //logo.setBounce(1, 1);
+        //logo.setCollideWorldBounds(true);
+    
+        //emitter.startFollow(logo);
     }
-
+    //Phaser
     update(){
         super.update();
     }
-
-
 
     draw() {
         super.draw();
